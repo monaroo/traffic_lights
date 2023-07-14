80a1b8e0fa64dc8d5ed0cb2938c487d7692c826c
Mona White <monarooster@gmail.com>
2023-07-14 12:50:07 -0600

    finish Light and grid modules & tests

--- lib/traffic_lights.ex
+++ lib/traffic_lights.ex
@@ -1,18 +1,86 @@
 defmodule TrafficLights do
-  @moduledoc """
-  Documentation for `TrafficLights`.
-  """
+  defmodule Light do
+    use GenServer
+    def change_color(state) do
+      case state do
+        :green -> :yellow
+        :yellow -> :red
+        :red -> :green
+      end
+    end
 
-  @doc """
-  Hello world.
+    @impl true
+    def init(_opts) do
+      {:ok, :green}
+    end
 
-  ## Examples
+    def start_link(_init_arg) do
+      GenServer.start_link(__MODULE__, :green)
+    end
 
-      iex> TrafficLights.hello()
-      :world
+    def current_light(pid) do
+      GenServer.call(pid, :current_light)
+    end
 
-  """
-  def hello do
-    :world
+    def transition(pid) do
+      GenServer.cast(pid, :transition)
+    end
+
+    @impl true
+    def handle_call(:current_light, _from, state) do
+      {:reply, state, state}
+    end
+
+    @impl true
+    def handle_cast(:transition, state) do
+      new_state = change_color(state)
+      {:noreply, new_state}
+    end
+
   end
+
+defmodule Grid do
+  use GenServer
+  alias TrafficLights.Light
+
+  def start_link(_init_arg) do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def init(_opts) do
+    pids = Enum.map(1..5, fn _ ->
+    {:ok, pid} = Light.start_link([])
+    pid
+    end)
+    {:ok, %{light_grid: pids, transition_index: 0}}
+  end
+
+  def transition(light_grid) do
+    GenServer.cast(light_grid, :transition)
+  end
+
+  def current_grid(light_grid) do
+    GenServer.call(light_grid, :current_grid)
+  end
+
+  def handle_cast(:transition, state) do
+
+    changing_light = Enum.at(state.light_grid, state.transition_index)
+    Light.transition(changing_light)
+
+    # grid = Enum.map(state.light_grid, &Light.current_light/1)
+    next_transition_index = rem(state.transition_index + 1, length(state.light_grid))
+
+    new_state = %{state | transition_index: next_transition_index}
+    {:noreply, new_state}
+  end
+
+  def handle_call(:current_grid, _from, state) do
+    grid = Enum.map(state.light_grid, &Light.current_light/1)
+
+    {:reply, grid, state}
+  end
+end
+
+
 end
--- test/traffic_lights_test.exs
+++ test/traffic_lights_test.exs
@@ -2,7 +2,71 @@ defmodule TrafficLightsTest do
   use ExUnit.Case
   doctest TrafficLights
 
-  test "greets the world" do
-    assert TrafficLights.hello() == :world
+  # TrafficLights.Light ----------------------------------
+  test "TrafficLights.Light.start_link/1 init state is green" do
+    {:ok, pid} = TrafficLights.Light.start_link([])
+    assert :sys.get_state(pid) == :green
+  end
+
+  test "current_light/1 functionality" do
+    {:ok, pid} = TrafficLights.Light.start_link([])
+    assert TrafficLights.Light.current_light(pid) == :green
+  end
+
+  test "transition/1 functionality" do
+    {:ok, pid} = TrafficLights.Light.start_link([])
+    TrafficLights.Light.transition(pid)
+    assert :sys.get_state(pid) == :yellow
+    TrafficLights.Light.transition(pid)
+    TrafficLights.Light.transition(pid)
+    assert :sys.get_state(pid) == :green
+  end
+
+  # TrafficLights.Grid -------------------------------------
+  test "initialize" do
+    {:ok, pid} = TrafficLights.Grid.start_link([])
+
+    assert TrafficLights.Grid.current_grid(pid) == [:green, :green, :green, :green, :green]
+  end
+
+  test "change 1 light" do
+    {:ok, pid} = TrafficLights.Grid.start_link([])
+    TrafficLights.Grid.transition(pid)
+    assert TrafficLights.Grid.current_grid(pid) == [:yellow, :green, :green, :green, :green]
+  end
+
+  test "change multiple lights" do
+    {:ok, pid} = TrafficLights.Grid.start_link([])
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    assert TrafficLights.Grid.current_grid(pid) == [:yellow, :yellow, :yellow, :yellow, :yellow]
+
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    assert TrafficLights.Grid.current_grid(pid) == [:red, :red, :red, :red, :red]
+  end
+
+  test "wrap around" do
+    {:ok, pid} = TrafficLights.Grid.start_link([])
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+    TrafficLights.Grid.transition(pid)
+
+    assert TrafficLights.Grid.current_grid(pid) == [:green, :red, :red, :red, :red]
+
   end
 end
